86402ba29567738aaa475604ffdf4cd3aa285832
Nora-Bouanani <Nora.Bouanani@student.hepl.be>
2023-11-21 19:23:04 +0100

    modif

---
+++ ex02/Step2/LibSerBL.c
@@ -0,0 +1,42 @@
+#include "LibSerBL.h"
+
+void AProposServeurBL(char *Version,char *Nom1,char* Nom2)
+{
+ printf("Version : %s \n",Version ) ;
+ printf("Nom1 : %s \n",Nom1 ) ;
+ printf("Nom2 : %s \n",Nom2 ) ;
+}
+
+//----------------ajout------------------------
+int RechercheBL(char* NomFichier,int Reference ,struct VehiculeBL *UnRecord) //ajout
+{
+    FILE *fichier ;
+
+    fichier = fopen(NomFichier, "r");
+    if (fichier == NULL) 
+    {
+        fprintf(stderr,"Echec Ouverture\n") ;
+        exit(0) ;
+    }
+    else
+        fprintf(stderr,"Ouverture reussie \n") ;
+
+
+    while (fread(UnRecord, sizeof(struct VehiculeBL), 1, fichier) == 1) 
+    {
+        if (UnRecord->Reference == Reference) 
+        {
+            AfficheEnteteVehiculeBL();
+            AfficheVehiculeBL(UnRecord);
+            fclose(fichier);
+            return 1; 
+        }
+        else
+        {
+            printf("Reference incorrect...\n");
+        }
+    }
+
+    fclose(fichier);
+    return 0;  
+}
---
+++ ex02/Step2/data.h
@@ -0,0 +1,29 @@
+#ifdef DATAH
+
+#else
+
+
+#define DATAH
+#include <time.h>
+// 2022 , interdit de modifier les champs existants
+
+struct VehiculeBL { 
+         int  Supprime ;
+         int  Reference ;
+	 char Constructeur[30] ;
+   	 char Modele[30] ;
+	 int  Quantite ;
+	 //----------------ajout------------------------
+	 int Portes; 
+      
+	} ;
+
+struct FactureBL
+       {
+         int NumeroFacturation ;
+         char Acheteur[40] ;
+	 time_t DateFacturation ;
+         int Quantite;
+	 int Reference ;
+	} ;  	
+#endif
